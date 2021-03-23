50fef306b7ed8f650fe92675242d65cd6ece3b5b
WytheHuang <893609357@qq.com>
2021-03-23 23:23:19 +0800

    mdf:refactor# 优化代码结构

--- execute.bat
+++ execute.bat
@@ -1,1 +1,1 @@
-go run ./main.go ./adb.go
+go run ./main.go
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -43,6 +46,43 @@ type Sign struct {
 	Coordinate Coordinate `json:"coordinate"`
 	Click      Click      `json:"click"`
 	Photo      Photo      `json:"photo"`
+}
+
+func AdbShellDumpsysPowerOff() bool {
+	flag := false
+	MyCmd := exec.Command("cmd.exe", "/c", "adb shell dumpsys power")
+	MyOut, _ := MyCmd.StdoutPipe()
+	MyCmd.Start()
+	MyBytes, _ := ioutil.ReadAll(MyOut)
+	MyCmd.Wait()
+	MyOut.Close()
+	s := string(MyBytes)
+	if strings.Contains(s, "Display Power: state=OFF") {
+		flag = true
+	}
+	return flag
+}
+
+func AdbShellInputTap(x, y int) {
+	x2 := strconv.Itoa(x)
+	y2 := strconv.Itoa(y)
+	exec.Command("adb", "shell", "input", "tap", x2, y2).Run()
+}
+
+func AdbShellInputSwipe(x1, y1, x2, y2 int) {
+	xx1 := strconv.Itoa(x1)
+	yy1 := strconv.Itoa(y1)
+	xx2 := strconv.Itoa(x2)
+	yy2 := strconv.Itoa(y2)
+	exec.Command("adb", "shell", "input", "swipe", xx1, yy1, xx2, yy2).Run()
+}
+
+func AdbShellInputKeyEvent(s string) {
+	exec.Command("adb", "shell", "input", "keyevent", s).Run()
+}
+
+func AdbShellInputText(s string) {
+	exec.Command("adb", "shell", "input", "text", s).Run()
 }
 
 func main() {
