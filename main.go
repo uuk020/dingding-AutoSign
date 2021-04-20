e8ace866f0f95256a7e54540eb4d4286e1aa1ab3
WytheHuang <893609357@qq.com>
2021-04-20 23:58:16 +0800

    mdf:refactor# 优化钉钉打卡逻辑和初始化配置文件

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"strconv"
@@ -48,17 +49,41 @@ type Sign struct {
 	Photo      Photo      `json:"photo"`
 }
 
-func AdbShellDumpsysPowerOff() bool {
+// func isDeviced() bool {
+// 	MyCmd := exec.Command("cmd.exe", "/c", "adb devices")
+// 	MyOut, err := MyCmd.CombinedOutput()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	s := string(MyOut)
+// 	var validDevice = regexp.MustCompile(`(?m:device$)`)
+// 	return validDevice.MatchString(s)
+// }
+
+func isPowerOff() bool {
 	flag := false
-	MyCmd := exec.Command("cmd.exe", "/c", "adb shell dumpsys power")
-	MyOut, _ := MyCmd.StdoutPipe()
-	MyCmd.Start()
-	MyBytes, _ := ioutil.ReadAll(MyOut)
-	MyCmd.Wait()
-	MyOut.Close()
-	s := string(MyBytes)
+	MyCmd := exec.Command("cmd.exe", "/c", "adb shell dumpsys power | findstr Display")
+	MyOut, err := MyCmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := string(MyOut)
 	if strings.Contains(s, "Display Power: state=OFF") {
 		flag = true
+	}
+	return flag
+}
+
+func isLock() bool {
+	flag := true
+	MyCmd := exec.Command("cmd.exe", "/c", "adb shell dumpsys window policy | findstr isStatusBarKeyguard")
+	MyOut, err := MyCmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := string(MyOut)
+	if strings.Contains(s, "isStatusBarKeyguard=false") {
+		flag = false
 	}
 	return flag
 }
@@ -86,10 +111,23 @@ func AdbShellInputText(s string) {
 }
 
 func main() {
-	fmt.Println("开启钉钉打卡~")
-	jsonFile, err := os.Open("config.json")
+	// 检查周几
+	day := time.Now().Weekday().String()
+	if day == time.Sunday.String() || day == time.Saturday.String() {
+		fmt.Println("双休日无须更新!")
+		os.Exit(0)
+	}
+
+	// if !isDeviced() {
+	// 	fmt.Println("没有连接")
+	// 	os.Exit(1)
+	// }
+
+	// 读取 config 配置
+	jsonFile, err := os.Open("C:\\Users\\Null\\Desktop\\DingAutoSign-master\\config.json")
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer jsonFile.Close()
 
@@ -98,32 +136,36 @@ func main() {
 	var config Config
 	json.Unmarshal([]byte(byteValue), &config)
 
+	fmt.Println("开启钉钉打卡~")
+
 	// 检查是否熄屏
-	if AdbShellDumpsysPowerOff() {
+	if isPowerOff() {
 		AdbShellInputKeyEvent("26") //power
-	}
-	// 滑动解锁
-	AdbShellInputSwipe(747, 1413, 747, 323)
-	if config.Lock.Require {
-		AdbShellInputText(config.Lock.Password)
-		AdbShellInputTap(config.Lock.Coordinate.X, config.Lock.Coordinate.Y)
+		if isLock() {
+			// 滑动解锁
+			AdbShellInputSwipe(747, 1413, 747, 323)
+			if config.Lock.Require {
+				AdbShellInputText(config.Lock.Password)
+				AdbShellInputTap(config.Lock.Coordinate.X, config.Lock.Coordinate.Y)
+			}
+		}
 	}
 	// 开启钉钉，若在钉钉界面则不需要
 	if config.StartDingding.Require {
 		AdbShellInputTap(config.StartDingding.Coordinate.X, config.StartDingding.Coordinate.Y)
-		time.Sleep(time.Duration(15) * time.Second)
+		time.Sleep(time.Duration(12) * time.Second)
 	}
 	// 钉钉工作台
 	AdbShellInputTap(config.WorkPlatform.Coordinate.X, config.WorkPlatform.Coordinate.Y)
-	time.Sleep(time.Duration(10) * time.Second)
+	time.Sleep(time.Duration(6) * time.Second)
 	// 钉钉打卡位置并打卡
 	AdbShellInputTap(config.Sign.Coordinate.X, config.Sign.Coordinate.Y)
-	time.Sleep(time.Duration(15) * time.Second)
+	time.Sleep(time.Duration(12) * time.Second)
 	AdbShellInputTap(config.Sign.Click.X, config.Sign.Click.Y)
 	// 需要拍照
 	if config.Sign.Photo.Require {
 		AdbShellInputTap(config.Sign.Photo.Coordinate.X, config.Sign.Photo.Coordinate.Y)
-		time.Sleep(time.Duration(5) * time.Second)
+		time.Sleep(time.Duration(6) * time.Second)
 		AdbShellInputTap(config.Sign.Photo.Coordinate.X, config.Sign.Photo.Coordinate.Y)
 	}
 	time.Sleep(time.Duration(15) * time.Second)
